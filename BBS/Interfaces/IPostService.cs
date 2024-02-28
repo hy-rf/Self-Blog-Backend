78d7428163d8f905977ca2fa7a4583cc231919ba
rf19990202 <heyu880202@gmail.com>
2024-02-28 17:20:45 +0800

    feat : create post

--- BBS/Interfaces/IPostService.cs
+++ BBS/Interfaces/IPostService.cs
@@ -4,6 +4,7 @@ namespace BBS.Interfaces
 {
     public interface IPostService
     {
+        public bool CreatePost(string Title, string Content, int UserId, string? Tags);
         public Post GetPost(int id);
         public IEnumerable<Post> GetRecentPosts();
         public IEnumerable<Post> GetMostReplyPosts();
--- BBS/Services/PostService.cs
+++ BBS/Services/PostService.cs
@@ -1,10 +1,40 @@
 ﻿using BBS.Interfaces;
 using BBS.Models;
+using Microsoft.Data.Sqlite;
 
 namespace BBS.Services
 {
     public class PostService : IPostService
     {
+        private readonly SqliteConnection Connection;
+        public PostService(IDatabase database)
+        {
+            Connection = database.SqLiteConnection();
+        }
+        public bool CreatePost(string Title, string Content, int UserId, string? Tags)
+        {
+            SqliteCommand CreatePostCommand = new SqliteCommand();
+            CreatePostCommand.Connection = Connection;
+            CreatePostCommand.CommandText = (string.IsNullOrEmpty(Tags)) ? @"INSERT INTO Post (Title, Content, UserId) VALUES ($Title, $Content, $UserId)" : @"INSERT INTO Post (Title, Content, UserId, Tags) VALUES ($Title, $Content, $UserId, $Tags)";
+            CreatePostCommand.Parameters.AddWithValue("$Title", Title);
+            CreatePostCommand.Parameters.AddWithValue("$Content", Content);
+            CreatePostCommand.Parameters.AddWithValue("$UserId", UserId);
+            if (string.IsNullOrEmpty(Tags))
+            {
+                CreatePostCommand.Parameters.AddWithValue("$Tags", Tags);
+            }
+            Connection.Open();
+            try
+            {
+                CreatePostCommand.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                Connection.Close();
+                return false;
+            }
+        }
         public Post GetPost(int id)
         {
             throw new NotImplementedException();
