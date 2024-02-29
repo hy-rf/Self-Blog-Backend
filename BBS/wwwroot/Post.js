ccf1fd55f3d5fded49bac4817a0c2a30258d0ab6
rf19990202 <heyu880202@gmail.com>
2024-02-29 14:39:36 +0800

    fix : improve readability

--- BBS/wwwroot/Post.js
+++ BBS/wwwroot/Post.js
@@ -1,7 +1,8 @@
 
 var PostForm = document.createElement("form");
 PostForm.setAttribute("method", "POST");
-PostForm.setAttribute("action", "Post/CreatePost");
+PostForm.setAttribute("asp-controller", "Post");
+PostForm.setAttribute("asp-action", "CreatePost");
 PostForm.innerHTML = `<input name="Title"><input name="Content"><input name="Tags"><button>Post</button>`;
 
 document.getElementById("Post").addEventListener("click", () => {
