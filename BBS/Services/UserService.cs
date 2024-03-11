32c4c0de5b2a43e4f2e297daf71ad424d43c1962
rf19990202 <heyu880202@gmail.com>
2024-03-11 11:24:53 +0800

    user moved to efcore

--- BBS/Services/UserService.cs
+++ BBS/Services/UserService.cs
@@ -8,58 +8,34 @@ namespace BBS.Services
 {
     public class UserService : IUserService
     {
-        private readonly SqliteConnection Connection;
-        private readonly IDatabase _database;
         private readonly AppDbContext _appDbContext;
         public int UserId { get; set; }
         public UserService(IDatabase database, AppDbContext appDbContext)
         {
-            Connection = database.SqLiteConnection();
-            _database = database;
             _appDbContext = appDbContext;
         }
         public int GetUserId()
         {
             return this.UserId;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <returns></returns>
         public bool Signup(string username, string password)
         {
             if (CheckSignup(username))
             {
-                var command = Connection.CreateCommand();
-                command.Connection = Connection;
-                command.CommandText = @"INSERT INTO User (Name, Pwd, Created, LastLogin, Avatar) VALUES ($Name, $Password, $Created, $LastLogin, $Avatar)";
-                command.Parameters.AddWithValue("$Name", username);
-                command.Parameters.AddWithValue("$Password", password);
-                command.Parameters.AddWithValue("$Created", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-                command.Parameters.AddWithValue("$LastLogin", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-                command.Parameters.AddWithValue("$Avatar", "");
-                Connection.Open();
-                try
+                var newUser = new User
                 {
-                    command.ExecuteNonQuery();
-                    Connection.Close();
-                    return true;
-                }
-                catch
-                {
-                    Connection.Close();
-                    return false;
-                }
+                    Name = username,
+                    Pwd = password,
+                    Created = DateTime.Now,
+                    LastLogin = DateTime.Now,
+                    Avatar = ""
+                };
+                _appDbContext.User.Add(newUser);
+                _appDbContext.SaveChanges();
+                return true;
             }
             return false;
         }
-        /// <summary>
-        /// check if username is being used
-        /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
         public bool CheckSignup(string username)
         {
             var Used = _appDbContext.User.Any(u => u.Name == username);
@@ -69,15 +45,9 @@ namespace BBS.Services
             }
             return true;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
-        /// <returns></returns>
         public bool Login(string username, string password)
         {
-            var Matched = _appDbContext.User.Where(u => u.Name == username && u.Pwd == password).Select(u => new {u.Id, u.Name}).ToList();
+            var Matched = _appDbContext.User.Where(u => u.Name == username && u.Pwd == password).Select(u => new { u.Id, u.Name }).ToList();
             if (Matched.Count != 0)
             {
                 this.UserId = Matched[0].Id;
@@ -87,93 +57,32 @@ namespace BBS.Services
                 return true;
             }
             return false;
-            // var command = Connection.CreateCommand();
-            // command.Connection = Connection;
-            // command.CommandText = @"SELECT Id, Name FROM User WHERE Name = $username AND Pwd = $password";
-            // command.Parameters.AddWithValue("$username", username);
-            // command.Parameters.AddWithValue("$password", password);
-            // Connection.Open();
-            // using (SqliteDataReader reader = command.ExecuteReader())
-            // {
-            //     if (reader.Read())
-            //     {
-            //         this.UserId = reader.GetInt32(0);
-            //         var updateLastLogin = Connection.CreateCommand();
-            //         updateLastLogin.Connection = Connection;
-            //         updateLastLogin.CommandText = @"UPDATE User SET LastLogin = $LastLogin WHERE Id = $Id";
-            //         updateLastLogin.Parameters.AddWithValue("LastLogin", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-            //         updateLastLogin.Parameters.AddWithValue("$Id", this.UserId);
-            //         updateLastLogin.ExecuteNonQuery();
-            //         Connection.Close();
-            //         return true;
-            //     }
-            // }
-            // Connection.Close();
-            // return false;
         }
-        /// <summary>
-        /// Used in UserCenter Page
-        /// </summary>
-        /// <param name="Id"></param>
-        /// <returns></returns>
         public User GetUser(int Id)
         {
-            var command = Connection.CreateCommand();
-            command.Connection = Connection;
-            command.CommandText = @"SELECT Id, Name, Created, LastLogin, Avatar FROM User WHERE Id = $Id";
-            command.Parameters.AddWithValue("$Id", Id);
-            User user = new User();
-            Connection.Open();
-            using SqliteDataReader reader = command.ExecuteReader();
-            if (reader.Read())
+            var User = _appDbContext.User.Where(u => u.Id == Id).Select(u => new User
             {
-                user = new User
-                {
-                    Id = reader.GetInt32(0),
-                    Name = reader.GetString(1),
-                    Created = reader.GetDateTime(2),
-                    LastLogin = reader.GetDateTime(3),
-                    Avatar = reader.GetString(4)
-                };
-            }
-            Connection.Close();
-            return user;
+                Id = u.Id,
+                Name = u.Name,
+                Created = u.Created,
+                LastLogin = u.LastLogin,
+                Avatar = u.Avatar
+            }).ToList();
+            return User[0];
         }
         public bool EditAvatar(int Id, string Avatar)
         {
-            SqliteCommand EditAvatar = new SqliteCommand
-            {
-                CommandText = @"UPDATE User SET Avatar = $Avatar WHERE Id = $Id",
-                Connection = Connection
-            };
-            EditAvatar.Parameters.AddWithValue("$Avatar", Avatar);
-            EditAvatar.Parameters.AddWithValue("$Id", Id);
-            Connection.Open();
-            if (EditAvatar.ExecuteNonQuery() != -1)
-            {
-                Connection.Close();
-                return true;
-            }
-            Connection.Close();
-            return false;
+            var EditAvatar = _appDbContext.User.Single(u => u.Id == Id);
+            EditAvatar.Avatar = Avatar;
+            _appDbContext.SaveChanges();
+            return true;
         }
         public bool EditName(int Id, string Name)
         {
-            SqliteCommand EditName = new SqliteCommand
-            {
-                CommandText = @"UPDATE User SET Name = $Name WHERE Id = $Id",
-                Connection = Connection
-            };
-            EditName.Parameters.AddWithValue("$Name", Name);
-            EditName.Parameters.AddWithValue("$Id", Id);
-            Connection.Open();
-            if (EditName.ExecuteNonQuery() != -1)
-            {
-                Connection.Close();
-                return true;
-            }
-            Connection.Close();
-            return false;
+            var EditName = _appDbContext.User.Single(u => u.Id == Id);
+            EditName.Name = Name;
+            _appDbContext.SaveChanges();
+            return true;
         }
     }
 }
