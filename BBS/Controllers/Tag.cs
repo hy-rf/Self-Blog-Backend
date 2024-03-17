ae6384fd0175da3994a98e0be42c20ebb39928c9
rf19990202 <heyu880202@gmail.com>
2024-03-17 10:33:24 +0800

    feat : tag list

---
+++ BBS/Controllers/Tag.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBS.Controllers
+{
+    public class Tag : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
