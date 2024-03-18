897418a4b74f7520d3f7aa3bddd428580f8892d8
rf19990202 <heyu880202@gmail.com>
2024-03-18 14:01:11 +0800

    #

--- BBS/wwwroot/Welcome.js
+++ BBS/wwwroot/Welcome.js
@@ -1,30 +1,30 @@
 
-//document.getElementById("loginBtn").addEventListener("click", () => {
-//    document.getElementById("togglePanel").innerHTML =
-//        `<form method="POST" action="/User/Login">
-//            <label for="Name">Name</label>
-//            <input id="Name" name="Name" required />
-//            <label for="Pwd">Pwd</label>
-//            <input id="Pwd" name="Pwd" type="password" required />
-//            <button type="submit">login</button>
-//         </form>`;
-//});
-
-//document.getElementById("signupBtn").addEventListener("click", () => {
-//    document.getElementById("togglePanel").innerHTML =
-//        `<form method="POST" action="/User/Signup">
-//            <label for="Name">Name</label>
-//            <input id="Name" name="Name" required />
-//            <label for="Pwd">Pwd</label>
-//            <input id="Pwd" name="Pwd" type="password" required />
-//            <button type="submit">sign up</button>
-//         </form>`;
-//});
-
 document.getElementById("loginBtn").addEventListener("click", () => {
-    document.getElementById("togglePanel").innerHTML = document.getElementsByTagName("template")[0].innerHTML;
+    document.getElementById("togglePanel").innerHTML =
+        `<form method="POST" action="/User/Login">
+            <label for="Name">Name</label>
+            <input id="Name" name="Name" required />
+            <label for="Pwd">Pwd</label>
+            <input id="Pwd" name="Pwd" type="password" required />
+            <button type="submit">login</button>
+         </form>`;
 });
 
 document.getElementById("signupBtn").addEventListener("click", () => {
-    document.getElementById("togglePanel").innerHTML = document.getElementsByTagName("template")[1].innerHTML;
+    document.getElementById("togglePanel").innerHTML =
+        `<form method="POST" action="/User/Signup">
+            <label for="Name">Name</label>
+            <input id="Name" name="Name" required />
+            <label for="Pwd">Pwd</label>
+            <input id="Pwd" name="Pwd" type="password" required />
+            <button type="submit">sign up</button>
+         </form>`;
 });
+
+//document.getElementById("loginBtn").addEventListener("click", () => {
+//    document.getElementById("togglePanel").innerHTML = document.getElementsByTagName("template")[0].innerHTML;
+//});
+
+//document.getElementById("signupBtn").addEventListener("click", () => {
+//    document.getElementById("togglePanel").innerHTML = document.getElementsByTagName("template")[1].innerHTML;
+//});
