689eacb82f2aeacd7f690aff3bcbb421492a9237
rf19990202 <heyu880202@gmail.com>
2024-03-18 16:14:11 +0800

    feat : get posts by tag

--- BBS/Controllers/TagController.cs
+++ BBS/Controllers/TagController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BBS.Data;
+using BBS.Interfaces;
+using BBS.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BBS.Controllers
 {
-    public class TagController : Controller
+    public class TagController(AppDbContext ctx) : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string tag)
         {
-            return View();
+            var postids = ctx.Tag.Select(t => new { t.Name, t.PostId }).Where(t => t.Name == tag);
+            List<Post> posts = new List<Post>();
+            foreach (var id in postids)
+            {
+                posts.Add(ctx.Post.Single(p => p.Id == id.PostId));
+            }
+            return View(posts);
         }
     }
 }
