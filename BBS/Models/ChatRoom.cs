b720239d26c11e9e5e63cb8f1cc501690a4272da
rf19990202 <heyu880202@gmail.com>
2024-03-21 13:42:33 +0800

    #

--- BBS/Models/ChatRoom.cs
+++ BBS/Models/ChatRoom.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Created { get; set; }
-        public List<ChatMessage> Messages { get; set; }
+        public List<ChatMessage> ChatMessages { get; set; }
+        public List<ChatRoomMember> ChatRoomMembers { get; set; }
     }
 }
