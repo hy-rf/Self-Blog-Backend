06755fbc753d022ce8e3f223c41b1f83ef9c6061
rf19990202 <heyu880202@gmail.com>
2024-03-24 16:54:22 +0800

    #

--- BBS/Interfaces/IFriendService.cs
+++ BBS/Interfaces/IFriendService.cs
@@ -1,6 +1,17 @@
-﻿namespace BBS.Interfaces
+﻿using BBS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBS.Interfaces
 {
     public interface IFriendService
     {
+        public void AddFriendRequest(FriendRequest friendRequest);
+        public void RemoveFriendRequest(FriendRequest friendRequest);
+        public List<FriendRequest> FriendRequests(int UserId);
+        public List<FriendRequest> FriendRequestsSent(int UserId);
+        public void AddFriend(Friend friend, Friend friendOpposite);
+        public void RemoveFriend(Friend friend, Friend friendOpposite);
+        public List<Friend> Friends(int UserId);
+        
     }
 }
--- BBS/Services/FriendService.cs
+++ BBS/Services/FriendService.cs
@@ -1,8 +1,48 @@
-﻿using BBS.Interfaces;
+﻿using BBS.Data;
+using BBS.Interfaces;
+using BBS.Models;
 
 namespace BBS.Services
 {
-    public class FriendService : IFriendService
+    public class FriendService(AppDbContext ctx) : IFriendService
     {
+        public void AddFriend(Friend friend, Friend friendOpposite)
+        {
+            ctx.Friend.Add(friend);
+            ctx.Friend.Add(friendOpposite);
+            ctx.SaveChanges();
+        }
+
+        public void AddFriendRequest(FriendRequest friendRequest)
+        {
+            ctx.FriendRequest.Add(friendRequest);
+            ctx.SaveChanges();
+        }
+
+        public List<FriendRequest> FriendRequests(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FriendRequest> FriendRequestsSent(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Friend> Friends(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFriend(Friend friend, Friend friendOpposite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFriendRequest(FriendRequest friendRequest)
+        {
+
+            throw new NotImplementedException();
+        }
     }
 }
