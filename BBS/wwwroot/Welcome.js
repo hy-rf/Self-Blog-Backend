f19a4a5294f758182a95f1091f549f9a8241c705
rf19990202 <heyu880202@gmail.com>
2024-03-26 15:09:48 +0800

    style : welcome page

--- BBS/wwwroot/Welcome.js
+++ BBS/wwwroot/Welcome.js
@@ -1,13 +1,13 @@
 
 document.getElementById("loginBtn").addEventListener("click", () => {
     document.getElementById("togglePanel").innerHTML =
-        `<form method="POST" action="/Login">
+        `<div>
             <label for="Name">Name</label>
-            <input id="Name" name="Name" required />
+            <input id="Name" name="Name" />
             <label for="Pwd">Pwd</label>
-            <input id="Pwd" name="Pwd" type="password" required />
-            <button type="submit">login</button>
-         </form>`;
+            <input id="Pwd" name="Pwd" type="password" />
+            <button id="submitLoginBtn" type="submit">login</button>
+        </div>`;
 });
 
 document.getElementById("signupBtn").addEventListener("click", () => {
@@ -17,7 +17,7 @@ document.getElementById("signupBtn").addEventListener("click", () => {
             <input id="Name" name="Name" required />
             <label for="Pwd">Pwd</label>
             <input id="Pwd" name="Pwd" type="password" required />
-            <button type="submit">sign up</button>
+            <button id="submitSignupBtn" type="submit">sign up</button>
          </form>`;
 });
 
@@ -28,3 +28,35 @@ document.getElementById("signupBtn").addEventListener("click", () => {
 //document.getElementById("signupBtn").addEventListener("click", () => {
 //    document.getElementById("togglePanel").innerHTML = document.getElementsByTagName("template")[1].innerHTML;
 //});
+
+
+document.getElementById("submitSignupBtn").addEventListener("click", async (e) => {
+    console.log("submitSignupBtn clicked");
+});
+
+
+
+document.getElementById("welcomeWrapper").addEventListener("click", async (e) => {
+    if (e.target.id == "submitLoginBtn") {
+        var res = await fetch("/api/User/Login", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                Name: document.getElementById("Name").value,
+                Pwd: document.getElementById("Pwd").value
+            })
+        }
+        ).then(response => {
+            return response.json();
+        });
+        if (res.success) {
+            window.location.href = "/UserCenter";
+        }
+        else {
+            document.getElementById("submitLoginBtn").innerText = "Login Failed";
+        }
+    }
+});
