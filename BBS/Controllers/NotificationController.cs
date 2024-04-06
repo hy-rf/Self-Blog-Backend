f95a27f592d2a61e2916cfd60f8c80a2a9d8aece
rf19990202 <152494023+rf19990202@users.noreply.github.com>
2024-04-06 18:10:07 +0800

    add notification controller

---
+++ BBS/Controllers/NotificationController.cs
@@ -0,0 +1,21 @@
+﻿using BBS.Common;
+using BBS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BBS.Controllers
+{
+    public class NotificationController : Controller
+    {
+        [HttpGet("Notifications")]
+        public async Task<JsonResult> Notifications(int UserId)
+        {
+            return Json(JsonBody.CreateResponse(false, "Feature Not Implemented"));
+        }
+        [HttpDelete("Notifications")]
+        public async Task<JsonResult> Delete([FromBody] Notification notification)
+        {
+            return Json(JsonBody.CreateResponse(false, "Feature Not Implemented"));
+        }
+    }
+}
