5700cc9c03ca26e923170e2763b55cf380f11fea
hy-rf <heyu880202@gmail.com>
2024-05-12 17:50:35 +0800

    Add PostListViewModel

---
+++ BBS/ViewModels/PostListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BBS.Models;
+
+namespace BBS.ViewModels
+{
+    public class PostListViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ContentPreview { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<User> UsersWhoLike { get; set; }
+    }
+}
